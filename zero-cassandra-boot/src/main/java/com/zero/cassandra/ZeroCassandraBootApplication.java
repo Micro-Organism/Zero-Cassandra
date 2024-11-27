fb5a8ed80969076ce6e8c6b73418b93dc4a7c189
Micro-Organism <coolrabbit1993@gmail.com>
2024-11-27 10:48:36 +0800

    feat: springboot 集成 cassandra

---
+++ zero-cassandra-boot/src/main/java/com/zero/cassandra/ZeroCassandraBootApplication.java
@@ -0,0 +1,13 @@
+package com.zero.cassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroCassandraBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroCassandraBootApplication.class, args);
+    }
+
+}
